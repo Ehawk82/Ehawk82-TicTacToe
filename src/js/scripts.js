4fc980301133784f46c2d598ddb8d6146073b0dd
Jason Graziano <mrehawk82@gmail.com>
2020-06-29 19:09:06 -0400

    ...

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,7 +5,27 @@ myUI = {
 		setTimeout(function(){myUI.loadout()},0); 
 	},
 	loadout: function(){
-		console.log("loadout");
+		var table = createEle("table");
+
+		table.className = "board";
+		for (var r = 0; r < 3; r++) {
+			var tr = createEle("tr"), j = r + 1;
+
+			tr.className = "r" + j;
+			for (var c = 0; c < 3; c++) {
+				var td = createEle("td"),d = c + 1;
+
+				td.className = "r" + j + " c" + d;
+				td.innerHTML = "&nbsp;";
+
+				tr.append(td);
+			}
+
+			table.append(tr);
+		}
+
+
+		body.append(table);
 	}
 };
 
