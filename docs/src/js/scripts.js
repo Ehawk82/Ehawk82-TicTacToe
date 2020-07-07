43281456001d4e3f912eb869a1d9a377318270a8
Jason Graziano <mrehawk82@gmail.com>
2020-07-06 20:23:50 -0400

    DRY patch

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -1,6 +1,23 @@
 var myUI,player = "X",winStatus = false,cellCount = 9;
 
+
 myUI = {
+	r1x: function(tds){ return tds[0].innerHTML === "X" && tds[1].innerHTML === "X" && tds[2].innerHTML === "X" },
+	r2x: function(tds){ return tds[3].innerHTML === "X" && tds[4].innerHTML === "X" && tds[5].innerHTML === "X" },
+	r3x: function(tds){ return tds[6].innerHTML === "X" && tds[7].innerHTML === "X" && tds[8].innerHTML === "X" },
+	c1x: function(tds){ return tds[0].innerHTML === "X" && tds[3].innerHTML === "X" && tds[6].innerHTML === "X" },
+	c2x: function(tds){ return tds[1].innerHTML === "X" && tds[4].innerHTML === "X" && tds[7].innerHTML === "X" },
+	c3x: function(tds){ return tds[2].innerHTML === "X" && tds[5].innerHTML === "X" && tds[8].innerHTML === "X" },
+	d1x: function(tds){ return tds[2].innerHTML === "X" && tds[4].innerHTML === "X" && tds[6].innerHTML === "X" },
+	d2x: function(tds){ return tds[0].innerHTML === "X" && tds[4].innerHTML === "X" && tds[8].innerHTML === "X" },
+	r1o: function(tds){ return tds[0].innerHTML === "O" && tds[1].innerHTML === "O" && tds[2].innerHTML === "O" },
+	r2o: function(tds){ return tds[3].innerHTML === "O" && tds[4].innerHTML === "O" && tds[5].innerHTML === "O" },
+	r3o: function(tds){ return tds[6].innerHTML === "O" && tds[7].innerHTML === "O" && tds[8].innerHTML === "O" },
+	c1o: function(tds){ return tds[0].innerHTML === "O" && tds[3].innerHTML === "O" && tds[6].innerHTML === "O" },
+	c2o: function(tds){ return tds[1].innerHTML === "O" && tds[4].innerHTML === "O" && tds[7].innerHTML === "O" },
+	c3o: function(tds){ return tds[2].innerHTML === "O" && tds[5].innerHTML === "O" && tds[8].innerHTML === "O" },
+	d1o: function(tds){ return tds[2].innerHTML === "O" && tds[4].innerHTML === "O" && tds[6].innerHTML === "O" },
+	d2o: function(tds){ return tds[0].innerHTML === "O" && tds[4].innerHTML === "O" && tds[8].innerHTML === "O" },
 	init: function(){
 		setTimeout(function(){myUI.loadout()},1); 
 	},
@@ -24,7 +41,6 @@ myUI = {
 
 			table.append(tr);
 		}
-
 
 		body.append(table,playerLabel);
 	},
@@ -51,24 +67,19 @@ myUI = {
 	evalBoard: function(tds){
 		--cellCount;
 		/* X row wins */
-		if (tds[0].innerHTML === "X" && tds[1].innerHTML === "X" && tds[2].innerHTML === "X" || tds[3].innerHTML === "X" && tds[4].innerHTML === "X" && tds[5].innerHTML === "X" || tds[6].innerHTML === "X" && tds[7].innerHTML === "X" && tds[8].innerHTML === "X" || tds[0].innerHTML === "X" && tds[3].innerHTML === "X" && tds[6].innerHTML === "X" || tds[1].innerHTML === "X" && tds[4].innerHTML === "X" && tds[7].innerHTML === "X" || tds[2].innerHTML === "X" && tds[5].innerHTML === "X" && tds[8].innerHTML === "X" || tds[0].innerHTML === "X" && tds[4].innerHTML === "X" && tds[8].innerHTML === "X" || tds[2].innerHTML === "X" && tds[4].innerHTML === "X" && tds[6].innerHTML === "X") {
+		if (myUI.r1x(tds) || myUI.r2x(tds) || myUI.r2x(tds) || myUI.c1x(tds) || myUI.c2x(tds) || myUI.c3x(tds) || myUI.d1x(tds) || myUI.d2x(tds)) {
 			myUI.xWin();
 			winStatus = true;
-		}
-
+		};
 		/* O row wins */
-		if (tds[0].innerHTML === "O" && tds[1].innerHTML === "O" && tds[2].innerHTML === "O" || tds[3].innerHTML === "O" && tds[4].innerHTML === "O" && tds[5].innerHTML === "O" || tds[6].innerHTML === "O" && tds[7].innerHTML === "O" && tds[8].innerHTML === "O" || tds[0].innerHTML === "O" && tds[3].innerHTML === "O" && tds[6].innerHTML === "O" || tds[1].innerHTML === "O" && tds[4].innerHTML === "O" && tds[7].innerHTML === "O" || tds[2].innerHTML === "O" && tds[5].innerHTML === "O" && tds[8].innerHTML === "O" || tds[0].innerHTML === "O" && tds[4].innerHTML === "O" && tds[8].innerHTML === "O" || tds[2].innerHTML === "O" && tds[4].innerHTML === "O" && tds[6].innerHTML === "O" ) {
+		if (myUI.r1o(tds) || myUI.r2o(tds) || myUI.r2o(tds) || myUI.c1o(tds) || myUI.c2o(tds) || myUI.c3o(tds) || myUI.d1o(tds) || myUI.d2o(tds)) {
 			myUI.oWin();
 			winStatus = true;
-		}
-
+		};
 		/* tie game */
 		if(cellCount === 0 && winStatus === false){
 			myUI.tieGame();
 		};
-		
-		
-		
 	},
 	xWin: function(){
 		var blokker = document.createElement("div"),
